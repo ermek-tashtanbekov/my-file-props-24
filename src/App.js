243c8abe979c01c0421c9f69666918e87e09ff58
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-02-24 21:57:26 +0600

    imported these references and values

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Age from "./componet/Age";
+import Book from "./componet/Book";
+import By from "./componet/By";
+import HelloWorld from "./componet/HelloWorld";
+import Hochu from "./componet/Hochu";
+import Lenght from "./componet/Lenght";
+import Sing from "./componet/Sing";
+import Swich from "./componet/Swich1";
+import Water from "./componet/Water";
+import Year from "./componet/Year";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <HelloWorld />
+    <By />
+    <Hochu v= "more" />
+    <Hochu v= "kosmost" />
+    <Book name= 'World' autor= 'Charlis  Darvin' />
+    <Sing sing= 'Mirbek' write= 'Toktogul Satulganpv'  />
+    <Lenght string= "Example string" />
+    <Lenght string= "Hello World" />
+    <Age year={1989} />
+    <Age year={1900} />
+    <Year age={21} />
+   <Swich on={false} />
+   <Swich on={true} />
+   <Water c ={-1} />
+   <Water c ={10} />
+   <Water c ={102} />
+
+    </div> 
   );
 }
 
